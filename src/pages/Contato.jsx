8589619ce45d1fb61453381a1a63f38f997ea76c
Marcus Vinícius <marcus-vnt@live.com>
2024-01-16 22:12:03 -0300

    Starting basic css.

---
+++ src/pages/Contato.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./Contato.module.css";
+import foto from "../img/contato.jpg";
+
+const Contato = () => {
+  return (
+    <section className={styles.contato}>
+      <img src={foto} alt="Máquina de Escrever" />
+      <div>
+        <h1>Entre em contato</h1>
+        <ul className={styles.dados}>
+          <li>marcus-vnt@live.com</li>
+          <li>(32) 99999-9999</li>
+          <li>Rua Logo Ali, 333</li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Contato;
